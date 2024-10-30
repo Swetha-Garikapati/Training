669f21dbb9df2136a2ed31a62729ac1f88d312aa
Swetha Garikapati <swethag@infinite.com>
2024-10-30 10:04:22 +0530

    Assignment submitted

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static int Calculator(int a, int b, out int sum, out int product,out int divide)
+        {
+            sum = a + b;  //addition
+            product = a * b;  //multiplication
+            divide = a / b;  //division
+            return a - b; // difference
+        }
+        static void Main(string[] args)
+        {
+            int Total, Prod, Div, Difference;
+
+            Difference =Calculator(12, 6, out Total, out Prod,out Div);
+            Console.WriteLine("Sum = {0} Product = {1} Div ={2} Difference = {3}", Total, Prod, Difference, Div);
+
+            Console.Read();
+            
+        }
+    }
+}
