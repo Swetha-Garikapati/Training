db567a72b0843039b67e44044df404b4e90a392d
Swetha Garikapati <swethag@infinite.com>
2025-01-02 18:58:41 +0530

    Assessment

---
+++ MVC/Assessments/Assessment-Q1/Controllers/CodeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assessment_1;
+
+namespace Assessment_1.Controllers
+{
+    public class CodeController : Controller
+    {
+            private northwindEntities db = new northwindEntities(); 
+
+            // Return all customers residing in Germany
+            public ActionResult CustomersInGermany()
+            {
+                var customers = db.Customers
+                                  .Where(c => c.Country == "Germany")
+                                  .ToList();
+                return View(customers);
+            }
+
+            //Return customer details with a specific OrderId (e.g., 10248)
+            public ActionResult CustomerByOrderId(int orderId)
+            {
+                var customer = db.Orders
+                                 .Where(o => o.OrderID == orderId)
+                                 .Select(o => o.Customer)
+                                 .FirstOrDefault();
+                return View(customer);
+            }
+    }
+}
