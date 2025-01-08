31072450e2e98279c2358d699d489da4dc92dc3a
Swetha Garikapati <swethag@infinite.com>
2025-01-08 11:13:11 +0530

    Assessment

---
+++ Web API/Assessment/App_Start/WebApiConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Assessment
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Enable attribute routing
+            config.MapHttpAttributeRoutes();
+
+            // Define the default route for Web API
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Web API/Assessment/Models/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assessment.Models
+{
+    public class Country
+    {
+        public int ID { get; set; }
+        public string CountryName { get; set; }
+        public string Capital { get; set; }
+    }
+}
